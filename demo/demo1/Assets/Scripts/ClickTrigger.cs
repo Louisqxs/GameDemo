a61e81273dcc7b6333a3073a2e6d29502d4e20a1
Louisqxs <514433824@qq.com>
2020-12-01 21:38:09 +0800

    update

---
+++ demo/demo1/Assets/Scripts/ClickTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ClickTrigger : MonoBehaviour
+{
+    public GameObject gameobject;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    public void Hide()
+    {
+        gameObject.GetComponent<CanvasGroup>().alpha = 0;
+        gameObject.transform.localScale = Vector3.zero;
+    }
+
+    public void Show()
+    {
+        gameObject.GetComponent<CanvasGroup>().alpha = 1;
+        gameObject.transform.localScale = Vector3.one;
+    }
+
+}
